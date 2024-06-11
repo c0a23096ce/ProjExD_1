1db69a34de3f3449914abeec08778a43b51d208d
eisho <c0a23096ce@edu.teu.ac.jp>
2024-06-11 16:53:17 +0900

    こうかとん操作

--- pygame_image.py
+++ pygame_image.py
@@ -13,7 +13,10 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img_rct = kk_img.get_rect() #こうかとんのrect抽出
+    kk_img_rct.center = 300, 200 #rectの中心位置
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -25,8 +28,15 @@ def main():
         #screen.blit(bg_img2, [-bg_move + 4800, 0])
         #bg_img_rct = bg_img.get_rect()
         #bg_img_rct.right = 0, 0 #rectの中心位置
-        kk_img_rct = kk_img.get_rect() #こうかとんのrect抽出
-        kk_img_rct.center = 300, 200 #rectの中心位置
+        key_get = pg.key.get_pressed()
+        if key_get[pg.K_UP]:
+            kk_img_rct.move_ip((0, -1))
+        elif key_get[pg.K_DOWN]:
+            kk_img_rct.move_ip((0, 1))
+        elif key_get[pg.K_LEFT]:
+            kk_img_rct.move_ip((-1, 0))
+        elif key_get[pg.K_RIGHT]:
+            kk_img_rct.move_ip((1, 0))
         screen.blit(kk_img, kk_img_rct)
         pg.display.update()
         tmr += 1     
